156afc72b30ab5bf267d1c31ac3e9848370f02be
caoweijian <caoweijian@kedacom.com>
2021-01-15 13:39:19 +0800

    修改程序入口

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 //聊天页面：https://segmentfault.com/a/1190000013712300
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/chatPage.dart';
 
 //程序入口
 void main() {
-  runApp(new FriendlychatApp());
+  runApp(new MaterialApp(
+    title: '首页',
+    home: new MainApp(),
+  ));
 }
 
-const String _name = "caowj"; //聊天帐号昵称
-
-class FriendlychatApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      //创建一个MaterialApp控件对象，其下可塞入支持Material设计语言特性的控件
-      title: "Friendlychat",
-      home: new Text("主体"), //主页面为用户自定义ChatScreen控件
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("主页面"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //跳转聊天页面
+              return new FriendlychatApp();
+            }));
+          },
+          child: Text("聊天"),
+        ),
+      ),
     );
   }
 }
